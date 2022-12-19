71460f4ae208e004256ee1802eff6151908ca1c6
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-12-19 21:06:49 +0530

    added downloable content along with flask-login for auth validations and logins

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
+import json
+
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-##CREATE TABLE IN DB
+
+# CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
+
+
+# Line below only required once, when creating DB.
 # db.create_all()
 
 
@@ -25,29 +39,58 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        if User.query.filter_by('email').first():
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        try:
+            hash_salted_pass = generate_password_hash(request.form['password'], method="pbkdf2:sha256", salt_length=8)
+            new_user = User(email=request.form.get('email'), name=request.form.get('name'), password=hash_salted_pass)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('secrets'))
+        except exc.IntegrityError as error:
+            return render_template("register.html", error=error)
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        if not check_password_hash(user.password, password):
+            flash("Incorrect Password, please try again!")
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name)
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory(directory='static', filename="files/cheat_sheet.pdf", as_attachment=True)
 
 
 @app.route('/logout')
 def logout():
-    pass
-
-
-@app.route('/download')
-def download():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
